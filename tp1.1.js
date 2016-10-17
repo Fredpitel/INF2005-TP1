417daa4fe10e05a1b2d2ab36f96bdaef4345f388
Fredpitel <fredpitel@gmail.com>
2016-10-17 18:02:43 -0400

    Pates et panier completer

--- tp1.1.js
+++ tp1.1.js
@@ -1,34 +1,110 @@
-var itemPanier;
+var panier;
 var sousTotal = 0.00;
+var itemsPanier = [];
 
 document.addEventListener('DOMContentLoaded', function () {
-	itemPanier = document.getElementById("listeItems");
+	panier = document.getElementById("listeItems");
 });
 
 function ajouterSpaghetti() {
 	var nbSpag = document.getElementById("nbSpag").value;
-	var sauceSpag = document.getElementById("sauceViandeSpag").checked;
+	var sauceSpag = document.getElementById("sauceSpag").checked;
 	var prixSpag;
+	var spag;
 	
 	if(nbSpag > 0) {
 		if(sauceSpag){
 			spag = "Spaghetti sauve à la viande";
-			prixSpag = 12.99 * nbSpag;
+			prixSpag = 12.99;
 		} else {
 			spag = "Spaghetti végétarien";
-			prixSpag = 10.99 * nbSpag;
+			prixSpag = 10.99;
 		}
-		itemPanier.innerHTML += "<div class=\"itemListe\">" + spag + "</div>" +
-								"<div class=\"quantiteItem\">" + nbSpag + "</div><br>" +
-								"<div class=\"prixItem\">" + prixSpag + "$</div><br><br>";
+		itemsPanier.push({item:spag, quantite:nbSpag, prix:(nbSpag * prixSpag)});
+		
+		mettrePanierAJour();
 	}
 	
-	modifierSousTotal(nbSpag * prixSpag);					 	
+	document.getElementById("nbSpag").value = 0;				 	
 }
 
-function modifierSousTotal(ajout) {
-	sousTotal += ajout;
-	document.getElementById("sousTotal").innerHTML = sousTotal + "$";
+function ajouterLasagne() {
+	var nbLasagne = document.getElementById("nbLasagne").value;
+	var sauceLasage = document.getElementById("sauceLasagne").checked;
+	var extra = document.getElementById("extraLasagne").checked;
+	var prixLasagne;
+	var lasagne;
+	
+	if(nbLasagne > 0) {
+		if(sauceLasage){
+			lasagne = "Lasagne sauve à la viande";
+			prixLasagne = 14.99;
+		} else {
+			lasagne = "Lasagne végétarienne";
+			prixLasagne = 12.99;
+		}
+		itemsPanier.push({item:lasagne, quantite:nbLasagne, prix:(nbLasagne * prixLasagne)});
+		if(extra){
+			itemsPanier.push({item:"Extra pepperoni", quantite:nbLasagne, prix:(1.99 * nbLasagne)});
+		}
+		
+		mettrePanierAJour();
+	}
+	
+	document.getElementById("nbLasagne").value = 0;
+	document.getElementById("extraLasagne").checked = false;			 	
+}
+
+function ajouterLinguini() {
+	var nbLin = document.getElementById("nbLin").value;
+	var sauceLin = document.getElementById("sauceLin").checked;
+	var prixLin;
+	var lin;
+	
+	if(nbLin > 0) {
+		if(sauceLin){
+			lin = "Linguini sauce rosée";
+			prixLin = 13.99;
+		} else {
+			lin = "Linguini aux fruits de mer";
+			prixLin = 19.99;
+		}
+		itemsPanier.push({item:lin, quantite:nbLin, prix:(nbLin * prixLin)});
+		
+		mettrePanierAJour();
+	}				 	
+	
+	nbLin = document.getElementById("nbLin").value = 0;
+}
+
+function mettrePanierAJour(){
+	panier.innerHTML = "";
+	
+	for(var i = 0; i < itemsPanier.length; i++){
+		panier.innerHTML += "<img class=\"croixRouge\" src=\"croixRouge.png\" id=\"" + i + "\" onclick=\"supprimerItem(this)\"></img>" +
+							"<div class=\"itemListe\">" + itemsPanier[i].item + "</div>" +
+							"<div class=\"quantiteItem\">" + itemsPanier[i].quantite + "</div><br>" +
+							"<div class=\"prixItem\">" + itemsPanier[i].prix + "$</div><br><br>";
+	}
+	
+	modifierSousTotal();	
+}
+
+function supprimerItem(item){	
+	if((itemsPanier.length > (parseInt(item.id) + 1)) && (itemsPanier[parseInt(item.id) + 1].item === "Extra pepperoni")){
+		itemsPanier.splice(parseInt(item.id) + 1, 1);
+	}
+	itemsPanier.splice(item.id, 1);
+	mettrePanierAJour();
+}
+
+function modifierSousTotal() {
+	sousTotal = 0.00;
+	
+	for(var i = 0; i < itemsPanier.length; i++){
+		sousTotal += itemsPanier[i].prix;
+	}
+	document.getElementById("sousTotal").innerHTML = sousTotal.toFixed(2) + "$";
 	
 	modififerTaxes();
 }
